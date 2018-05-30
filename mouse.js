4e6ef01af5ccc3ab479ff8cdff867721bb831293
Douhaku.R <ryuuji@christina.local>
2018-05-30 12:35:07 +0900

    add tab

--- mouse.js
+++ mouse.js
@@ -1,7 +1,7 @@
-window.document.onmousemove = myGetEvent; // マウスを動かした。
+window.document.onmousemove = myGetEvent; 
 function myGetEvent( evt ){ 
-  myEvent = ( evt ) ? evt : window.event; // FireFox 対策 
+  myEvent = ( evt ) ? evt : window.event; 
   myObj=document.getElementById("cursor").style;
-  myObj.left = document.body.scrollLeft + myEvent.clientX + 15; // X表示位置 
-  myObj.top = document.body.scrollTop + myEvent.clientY + 15; // Y表示位置 
+  myObj.left = document.body.scrollLeft + myEvent.clientX + 15; 
+  myObj.top = document.body.scrollTop + myEvent.clientY + 15; 
 } 
--- tab.js
+++ tab.js
@@ -1,7 +1,28 @@
-$(".tab_label").on("click",function(){
-	var $th = $(this).index();
-	$(".tab_label").removeClass("active");
-	$(".tab_panel").removeClass("active");
-	$(this).addClass("active");
-	$(".tab_panel").eq($th).addClass("active");
-});
+var tabs = document.getElementById('tab_control').getElementsByTagName('a');
+var pages = document.getElementById('tab_body').getElementsByTagName('div');
+
+function changeTab() {
+   var targetid = this.href.substring(this.href.indexOf('#')+1,this.href.length);
+
+   for(var i=0; i<pages.length; i++) {
+      if( pages[i].id != targetid ) {
+         pages[i].style.display = "none";
+      }
+      else {
+         pages[i].style.display = "block";
+      }
+   }
+
+   for(var i=0; i<tabs.length; i++) {
+      tabs[i].style.zIndex = "0";
+   }
+   this.style.zIndex = "10";
+
+   return false;
+}
+
+for(var i=0; i<tabs.length; i++) {
+   tabs[i].onclick = changeTab;
+}
+
+tabs[0].onclick();
