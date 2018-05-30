84ad042237659f1197aafe1e59e2d510315796d3
Douhaku.R <ryuuji@christina.local>
2018-05-30 16:02:27 +0900

    fix fish

--- mouse.js
+++ mouse.js
@@ -1,6 +1,7 @@
 window.document.onmousemove = myGetEvent; 
-function myGetEvent(){  
-  myObj=document.getElementById('cursor').style;
-  myObj.left = document.body.scrollLeft + event.clientX + 15; 
-  myObj.top = document.body.scrollTop + event.clientY + 15; 
+function myGetEvent(event){
+  myEvent = (event)?event:window.event;
+  myObj=document.getElementById("cursor").style;
+  myObj.left = document.body.scrollLeft + myEvent.clientX + 15; 
+  myObj.top = document.body.scrollTop + myEvent.clientY + 15; 
 } 
