96c67d9223acef3256d5443e5369286875ca05ee
Douhaku.R <ryuuji@christina.local>
2018-05-30 16:15:37 +0900

    fix fish-2

--- mouse.js
+++ mouse.js
@@ -1,7 +1,7 @@
-window.document.onmousemove = myGetEvent; 
-function myGetEvent(event){
-  myEvent = (event)?event:window.event;
+window.document.onmousemove = myGetEvent; // マウスを動かした。
+function myGetEvent( evt ){ 
+  myEvent = ( evt ) ? evt : window.event; // FireFox 対策 
   myObj=document.getElementById("cursor").style;
-  myObj.left = document.body.scrollLeft + myEvent.clientX + 15; 
-  myObj.top = document.body.scrollTop + myEvent.clientY + 15; 
+  myObj.left = document.body.scrollLeft + myEvent.clientX + 15; // X表示位置 
+  myObj.top = document.body.scrollTop + myEvent.clientY + 15; // Y表示位置 
 } 
