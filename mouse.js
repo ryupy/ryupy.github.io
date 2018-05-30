7ff2827530bce5e563f0738dfc699cbc3cdd9cf1
Douhaku.R <ryuuji@christina.local>
2018-05-30 15:17:31 +0900

    fix font-family-11

--- mouse.js
+++ mouse.js
@@ -1,6 +1,7 @@
 window.document.onmousemove = myGetEvent; 
-function myGetEvent(){ 
-  myObj=document.getElementById("cursor").style;
+function myGetEvent( evt ){ 
+  myEvent = ( evt ) ? evt : window.event;
+  myObj=document.getElementById('cursor').style;
   myObj.left = document.body.scrollLeft + myEvent.clientX + 15; 
   myObj.top = document.body.scrollTop + myEvent.clientY + 15; 
 } 
